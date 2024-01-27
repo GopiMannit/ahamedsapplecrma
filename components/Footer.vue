ae34eced30293a0ff7faa229878a7789884875c2
GOpiMannit <gopivijayangeetha8102@gmail.com.com>
2024-01-27 23:25:19 +0530

    First commit

---
+++ components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer>
+    <div>
+      <p>&copy; 2024 MANNIT-INNOVATIONS</p>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+  /* Add styling for your footer here */
+  footer {
+    background-color: none;
+    color: rgb(0, 0, 0);
+    margin-top: auto;
+    text-align: center;
+    font-weight: bolder;
+  }
+</style>
